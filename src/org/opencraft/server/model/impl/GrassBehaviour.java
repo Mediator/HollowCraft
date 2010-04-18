3c025672135885426d723771cdbb70682198d676
Trever Fischer <wm161@wm161.net>
2010-04-18 18:36:54 -0400

    Improved grass growth

--- src/org/opencraft/server/model/impl/GrassBehaviour.java
+++ src/org/opencraft/server/model/impl/GrassBehaviour.java
@@ -43,6 +43,22 @@ import org.opencraft.server.model.Level;
  * @author Brett Russell
  */
 public class GrassBehaviour implements BlockBehaviour {
+
+	// Represents the different directions grass can spread.
+	// Grass will one block in all directions around the grass
+	// block except directly above and below the block.
+	// x, y, z
+	public static final int[][] spreadRules = { { 1,  0, 0 }, { -1,  0, 0 }, {  0,   0, 1 }, { 0,  0, -1 },
+						    { 1,  0, 1 }, { -1,  0, 1 }, { -1,   0, 1 }, { 1,  0, -1 },
+						    { 1,  1, 0 }, { -1,  1, 0 }, {  0,   1, 1 }, { 0,  1, -1 },
+						    { 1,  1, 1 }, { -1,  1, 1 }, { -1,   1, 1 }, { 1,  1, -1 },
+						    { 1, -1, 0 }, { -1, -1, 1 }, {  0,  -1, 1 }, { 0, -1, -1 },
+						    { 1, -1, 1 }, { -1, -1, 1 }, { -1,  -1, 1 }, { 1, -1, -1 },
+						  };
+
+	public static final int[][] lifeLights = { { 1, 1,  0 }, { -1, 1,  0 }, {  0,  1,  1 }, { 0, 1,  -1 },
+						   { 1, 1,  1 }, { -1, 1,  1 }, { -1,  1,  1 }, { 1, 1,  -1 },
+						 };
 	
 	@Override
 	public void handleDestroy(Level level, int x, int y, int z, int type) {
@@ -57,27 +73,44 @@ public class GrassBehaviour implements BlockBehaviour {
 	@Override
 	public void handleScheduledBehaviour(Level level, int x, int y, int z, int type) {
 		// do we need to die?
-		if (level.getLightDepth(x, y) > z) {
+
+		byte aboveBlock = level.getBlock(x, y, z + 1);
+		// Should the block above us kill us?
+		if (BlockManager.getBlockManager().getBlock(aboveBlock).doesBlockLight() || BlockManager.getBlockManager().getBlock(aboveBlock).isLiquid()) {
 			level.setBlock(x, y, z, BlockConstants.DIRT);
 			return;
 		}
+
+		boolean lightFound = false;
+
+		SEARCH_LIGHT: for (int i = 0; i <= spreadRules.length - 1; i++) {
+			if (level.getLightDepth(x + spreadRules[i][0], y + spreadRules[i][1]) < z + spreadRules[i][2]) {
+				//we found light
+				lightFound = true;
+				break;
+			}
+		}
 		
-		// represents the different directions grass can spread
-		// x, y, z
-		int[][] spreadRules = { { 1, 0, 0 }, { -1, 0, 0 }, { 0, 1, 0 }, { 0, -1, 0 } };
-		
+		if (!lightFound) {
+			 level.setBlock(x, y, z, BlockConstants.DIRT);
+			 return;
+		}
+
 		// spread
-		for (int i = 0; i <= spreadRules.length - 1; i++) {
-			boolean found = false;
-			if (level.getBlock(x + spreadRules[i][0], y + spreadRules[i][1], z + spreadRules[i][2]) == BlockConstants.DIRT) {
-				if (z + spreadRules[i][2] < level.getLightDepth(x + spreadRules[i][0], y + spreadRules[i][1])) {
-					found = true;
-				}
-				if (BlockManager.getBlockManager().getBlock(level.getBlock(x + spreadRules[i][0], y + spreadRules[i][1], z + 1)).isLiquid()) {
-					found = true;
-				}
-				
-				if (!found) {
+		SEARCH_GRASS: for (int i = 0; i <= spreadRules.length - 1; i++) {
+			if (level.getLightDepth(x, y) < z) {
+				if (level.getBlock(x + spreadRules[i][0], y + spreadRules[i][1], z + spreadRules[i][2]) == BlockConstants.DIRT) {
+					// Is the block above it liquid?
+					if (BlockManager.getBlockManager().getBlock(level.getBlock(x + spreadRules[i][0], y + spreadRules[i][1], z + spreadRules[i][2] + 1)).isLiquid()) {
+						break SEARCH_GRASS;
+					}
+
+					// Is the block smothered by something?
+					if (BlockManager.getBlockManager().getBlock(level.getBlock(x + spreadRules[i][0], y + spreadRules[i][1], z + spreadRules[i][2] + 1)).doesBlockLight()) {
+						break SEARCH_GRASS;
+					}
+					
+					// Grow the grass
 					level.setBlock(x + spreadRules[i][0], y + spreadRules[i][1], z + spreadRules[i][2], BlockConstants.GRASS);
 				}
 			}
