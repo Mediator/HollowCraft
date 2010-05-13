318905a4ed94aabf9de0cca68ea4c5ff73e7464c
Trever Fischer <wm161@wm161.net>
2010-05-13 15:15:35 -0400

    Fix permissions glitch

--- src/org/opencraft/server/security/Role.java
+++ src/org/opencraft/server/security/Role.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 
 public class Role extends Permission {
 	private String m_name;
-	private boolean m_negative;
 
 	private ArrayList<Permission> m_permissions = new ArrayList<Permission>();
 
 	public Role(String name) {
 		super(name);
-		m_negative = false;
+	}
+
+	public String toString() {
+		return "@"+super.toString();
 	}
 
 	public Permission[] getPermissions() {
@@ -21,19 +23,11 @@ public class Role extends Permission {
 		m_permissions.add(p);
 	}
 
-	public void setNegative(boolean b) {
-		m_negative = b;
-	}
-
-	public boolean isNegative() {
-		return m_negative;
-	}
-
 	public boolean implies(Permission p) {
 		for(Permission sub : m_permissions) {
 			if (sub.implies(p))
-				return !m_negative;
+				return true;
 		}
-		return !m_negative;
+		return false;
 	}
 }
