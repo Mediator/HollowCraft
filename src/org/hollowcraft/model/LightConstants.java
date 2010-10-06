b7e19f03177b27885b414722286273a3fd08a8b3
Caleb Champlin <cmediator@gmail.com>
2010-10-06 17:19:23 -0600

    Add some constants for lighting for future use

---
+++ src/org/hollowcraft/model/LightConstants.java
@@ -0,0 +1,40 @@
+package org.hollowcraft.model;
+/*
+ * HollowCraft License
+ *  Copyright (c) 2010 Caleb Champlin.
+ *  All rights reserved
+ *  This license must be include in all copied, cloned and derived works 
+ */
+/**
+ * Lighting constants
+ * @author Caleb champlin
+ */
+public class LightConstants {
+	/**
+	 * Default private constructor.
+	 */
+	private LightConstants() {
+		/* empty */
+	}
+	public static final int DARK = 0;
+	public static final int SUBTLELIGHT = 1;
+	public static final int MEDIUMSUBTLELIGHT = 2;
+	public static final int HIGHSUBTLELIGHT = 3;
+	public static final int LOWLIGHT = 4;
+	public static final int MEDIUMLOWLIGHT = 5;
+	public static final int HIGHLOWLIGHT = 6;
+	public static final int SOFTLIGHT = 7;
+	public static final int MEDIUMSOFTLIGHT = 8;
+	public static final int HIGHSOFTLIGHT = 9;
+	public static final int MIDLIGHT = 10;
+	public static final int MEDIUMMIDLIGHT = 11;
+	public static final int HIGHMIDLIGHT = 12;
+	public static final int BRIGHTLIGHT = 13;
+	public static final int MEDIUMBRIGHTLIGHT = 14;
+	public static final int HIGHBRIGHTLIGHT = 15;
+	public static final int TORCHLIGHT = 14;
+	public static final int FURNACELIGHT = 14;
+	public static final int FIRELIGHT = 15;
+	public static final int SUNLIGHT = 15;
+	
+}
