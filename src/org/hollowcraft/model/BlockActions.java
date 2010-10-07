4a016f876430f9c23d202ed8ff0c1b2e73816ce0
Caleb Champlin <cmediator@gmail.com>
2010-10-06 21:09:39 -0600

    Add a constants class for block actions (atm only two: destory and build)

---
+++ src/org/hollowcraft/model/BlockActions.java
@@ -0,0 +1,17 @@
+package org.hollowcraft.model;
+/*
+ * HollowCraft License
+ *  Copyright (c) 2010 Caleb Champlin.
+ *  All rights reserved
+ *  This license must be include in all copied, cloned and derived works 
+ */
+
+/**
+ * A class which holds block id constants.
+ * @author Caleb Champlin
+ */
+public class BlockActions {
+	private BlockActions() { }
+	public static final int DESTROY = 0;
+	public static final int BUILD = 1;
+}
